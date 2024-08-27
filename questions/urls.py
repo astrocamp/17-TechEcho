287d0ed26c29bbadbda3e913430ff2c2fb200ffd
chen weichen <newsforcwc@gmail.com>
2024-08-27 09:53:24 +0800

    fix: fix 'questions:edit' url

--- questions/urls.py
+++ questions/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("", views.index, name="views"),
     path("new", views.new, name="new"),
     path("<int:id>", views.show, name="show"),
-    path("<int:id>", views.edit, name="edit"),
+    path("<int:id>/edit", views.edit, name="edit"),
 ]
